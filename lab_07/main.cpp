11ed8c53ea985b6da55e4feee39baff35d28ebb9
chrislvt <chrislvt@yandex.ru>
2020-05-18 05:26:32 +0300

    lab_07 added

---
+++ lab_07/main.cpp
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <string.h>
+#include <inttypes.h>
+
+#include <iostream>
+
+using namespace std;
+
+extern "C"
+{
+    int lvt_strcpy(const char* dest, const char* src, unsigned long long int len);
+}
+
+int main()
+{
+    string src, dst;
+    unsigned long long int len;
+
+    cout << "Input your string: ";
+    getline(cin, src);
+
+    __asm__(".intel_syntax noprefix     \n\t"
+            "xor  rax, rax              \n\t"
+
+            "cld                        \n\t"
+
+            "xor  rcx, rcx              \n\t"
+            "dec  rcx                   \n\t"
+
+            "repne   scasb              \n\t"
+            "not     rcx                \n\t"
+            "dec     rcx                \n\t"
+        :"=c"(len)
+        :"D"(src.c_str())
+        :"rax");
+
+    cout << "Length of " << src << " is " << len << endl;
+
+    dst.resize(src.length());
+    lvt_strcpy(&src[0], &src[1], 4);
+
+    cout << "Source string: " << src << endl;
+    cout << "Destination string: " << src << endl;
+
+    return 0;
+}
